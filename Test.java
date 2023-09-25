822fa2fffa9cc1b77c16f0df23a227e46e83eea5
Paul De Ulloa <pauld143@gmail.com>
2023-09-24 21:29:23 -0700

    Java fundamentals

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+
+//* File and class names must match.
+//* The file name and class name need to be exactly the same, or it won’t compile. In this case, Test.java and class Test
+
+//* File and class names should be Pascal case.
+//* The file name and class name should be written Pascal case. It should start with an uppercase letter and start each word with another capital letter. For example, a class file with all the methods (functions) for a bank account would be called BankAccount.java and class BankAccount
+
+//!Every time you make a change to your code, you will need to re-compile it.
